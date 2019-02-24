07aff43ca6d3a2b530d8f5723427c2d88ffea967
shunsuke akita <shunsukeakita@Shunsuke-PC.local>
2019-02-24 23:47:03 +0900

    initial commit:)

---
+++ R/hello-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' Title of the simple function to add 1
+#'
+#' The second paragraph is recognized as the description.
+#' It is not recommended to use @@title and @@description explicitly.
+#' @param x A numeric vector
+#' @export
+#' @examples
+#' increment(42)
+increment = function(x) {x + 1}
